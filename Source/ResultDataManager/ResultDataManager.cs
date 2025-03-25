119d6c915e8b3b0ddc8773909e66c7007aa7009e
Manish-SDU <manishmoriche@gmail.com>
2025-03-25 09:49:57 +0100

    RDM added

---
+++ Source/ResultDataManager/ResultDataManager.cs
@@ -0,0 +1,28 @@
+using System.Globalization;
+using CsvHelper;
+
+namespace DanfossHeating;
+
+public class ResultDataManager
+{
+    private readonly string _filePath = "Data/result_data.csv";
+
+    public void SaveResults(List<ResultEntry> results)
+    {
+        using var writer = new StreamWriter(_filePath, append: false); // Append to false (overwrite)
+        using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+        csv.WriteHeader<ResultEntry>(); // Write headers for new data
+        csv.NextRecord();
+        csv.WriteRecords(results);
+    }
+
+    public List<ResultEntry> LoadResults()
+    {
+        if (!File.Exists(_filePath)) return new List<ResultEntry>();
+
+        using var reader = new StreamReader(_filePath);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+        return [.. csv.GetRecords<ResultEntry>()];
+    }
+}
