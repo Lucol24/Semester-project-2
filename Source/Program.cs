ccedabb4188bb1b340bd74eb9ed6d881497fe358
chaeyrie <carolinafnunesamf@gmail.com>
2025-03-31 13:54:06 +0200

    s1 works as it should :)

--- Source/Program.cs
+++ Source/Program.cs
@@ -12,7 +12,25 @@ class Program
 
         Console.WriteLine("\nInitializing Optimizer...\n");
         ResultDataManager resultDataManager = new();
+        
+        Console.WriteLine("Summer or Winter?");
+        string? season = Console.ReadLine()?.Trim().ToLower() == "summer" ? "summer" : "winter";
+
+        /* Console.WriteLine("Scenario 1 or Scenario 2?");
+        bool IsScenario2 = Console.ReadLine()?.Trim().ToLower() == "2"; */
+
+        Console.WriteLine("Cost or CO2?");
+        string? criteriaInput = Console.ReadLine();
+
+        var criteria = Optimizer.OptimizationCriteria.Cost;
+        if (criteriaInput == "2") // Cost is 1 so CO2 is 2
+        {
+            criteria = Optimizer.OptimizationCriteria.CO2Emissions;
+        }
+
         Optimizer optimizer = new(assetManager, sourceDataManager, resultDataManager);
+        optimizer.OptimizeHeatProduction(season, criteria);
+    
 
         
     }
