9c0cdcc244efd436377002cc6f807d6c6dd8fcf2
Gabbo693 <gabrielesolazzo43@gmail.com>
2025-04-17 12:03:25 +0200

    First impkementation of the Cost Graph and gitignore for tests
    Co-authored-by: Lara-Ghi <Lara-Ghi@users.noreply.github.com>

--- DanfossHeating/Views/CostPage.axaml.cs
+++ DanfossHeating/Views/CostPage.axaml.cs
@@ -3,14 +3,17 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using DanfossHeating.ViewModels;
 using System;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
 
 namespace DanfossHeating.Views;
 
 public partial class CostPage : PageBase
 {
-    public CostPage()
+    public CostPage(string userName, bool isDarkTheme)
     {
         InitializeComponent();
+        DataContext = new CostViewModel(userName, isDarkTheme);
         Console.WriteLine("CostPage constructed");
     }
 
